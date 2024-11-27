d2c5d260eac85e6d5f24f3d0e45578528bf1d52c
rakib3719 <bannah76769@gmail.com>
2024-11-27 11:10:55 +0600

    df

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,12 @@
 import TopDestination from "./topDestination";
 
-
 const Home = () => {
-
-    return (
-      <div>
+  return (
+    <div>
       <h1 className="text-5xl">Roamify</h1>
       <TopDestination></TopDestination>
     </div>
-    )
+  );
 };
 
 export default Home;
