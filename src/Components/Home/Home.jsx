554aa4b37ea85848bd7b1c0847a9ab8facd7dd22
rakib3719 <bannah76769@gmail.com>
2024-11-27 11:09:16 +0600

    conflict solved

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,13 @@
 import TopDestination from "./topDestination";
 
-import Footer from "../Shared/Footer";
-import Navbar from "../Shared/Navbar";
 
 const Home = () => {
 
     return (
-        <div>
-            
-<Navbar/>
-
-<h1 className="text-5xl">Roamify</h1>
-<TopDestination></TopDestination>
-
-
-            <Footer/>
-        </div>
+      <div>
+      <h1 className="text-5xl">Roamify</h1>
+      <TopDestination></TopDestination>
+    </div>
     )
 };
 
