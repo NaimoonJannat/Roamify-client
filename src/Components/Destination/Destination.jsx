ed6c224545060a4aa26d6e199ef7bc3fdaf0d0ae
NaimoonJannat <naimoonjannat@gmail.com>
2024-11-28 13:20:48 +0600

    Destination page ui

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -1,11 +1,37 @@
-import Loader from "../Loader/Loader";
+
 import HeaderBanner from "../Shared/HeaderBanner";
+import SingleDestination from "./SingleDestination";
 
 const Destination = () => {
+    const destinations = [
+        {
+          id: 1,
+          name: "Canada",
+          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
+        },
+        {
+          id: 2,
+          name: "Maldivs",
+          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
+        },
+        {
+          id: 3,
+          name: "London",
+          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
+        },
+      ];
+    
     return (
         <div>
-            <HeaderBanner title="Find Your Destination" subtitle="Explore The Destination You Need"></HeaderBanner>
-            
+            <HeaderBanner title="Popular Destination" subtitle="Choose Your Destination"></HeaderBanner>
+            {/* Destination gallary  */}
+            <div className="w-full md:w-11/12 lg:w-4/5 mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {destinations.map((destination) => (
+            <SingleDestination destination={destination} key={destination.id} />
+        ))}
+            </div>
+            </div>
         </div>
     );
 };
