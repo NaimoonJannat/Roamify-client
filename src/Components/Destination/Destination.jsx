d13c0d88873e34fa4684be4f0db66b9d21ef6d82
NaimoonJannat <naimoonjannat@gmail.com>
2024-11-28 14:27:29 +0600

    Destinations functional from backend

--- src/Components/Destination/Destination.jsx
+++ src/Components/Destination/Destination.jsx
@@ -1,36 +1,27 @@
-
+import React from "react";
+import useFetch from "../../Hooks/useFetch";
 import HeaderBanner from "../Shared/HeaderBanner";
 import SingleDestination from "./SingleDestination";
+import Loader from "../Loader/Loader";
 
 const Destination = () => {
-    const destinations = [
-        {
-          id: 1,
-          name: "Canada",
-          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
-        },
-        {
-          id: 2,
-          name: "Maldivs",
-          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
-        },
-        {
-          id: 3,
-          name: "London",
-          image: "https://i.ibb.co/BB7rM6X/photo-1546412414-e1885259563a.jpg",
-        },
-      ];
-    
+    // Use the custom hook to fetch destinations
+    const { data: destinations, loading, error } = useFetch("http://localhost:5000/api/destinations");
+
     return (
         <div>
-            <HeaderBanner title="Popular Destination" subtitle="Choose Your Destination"></HeaderBanner>
-            {/* Destination gallary  */}
+            <HeaderBanner title="Popular Destination" subtitle="Choose Your Destination" />
+            {/* Destination gallery */}
             <div className="w-full md:w-11/12 lg:w-4/5 mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-            {destinations.map((destination) => (
-            <SingleDestination destination={destination} key={destination.id} />
-        ))}
-            </div>
+                {loading && <Loader></Loader>}
+                {error && <p className="text-red-500">{error}</p>}
+                {!loading && !error && (
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                        {destinations.map((destination) => (
+                            <SingleDestination destination={destination} key={destination.id} />
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
